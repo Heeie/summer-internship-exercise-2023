484c06e7cf44076b5f8f603c0ffad5159bb12ff8
Heeie <114531189+Heeie@users.noreply.github.com>
2023-06-11 18:55:44 +0100

    Update SnailShellPattern.java

--- src/main/java/com/premiumminds/internship/snail/SnailShellPattern.java
+++ src/main/java/com/premiumminds/internship/snail/SnailShellPattern.java
@@ -14,6 +14,87 @@ class SnailShellPattern implements ISnailShellPattern {
    * @return order array of values thar represent a snail shell pattern
    */
   public Future<int[]> getSnailShell(int[][] matrix) {
-    throw new RuntimeException("Not Implemented Yet");
+    ExecutorService executor = Executors.newSingleThreadExecutor();
+
+        Callable<int[]> task = new Callable<int[]>(){
+            int[] resultado = new int[matrix.length * matrix.length];
+            public int[] call() throws Exception {
+        
+                int count = matrix.length * matrix.length;
+        
+                int[][] vazio = new int[][] {{}};
+                
+                
+                //Verifica se a matriz é vazia
+                if(vazio == matrix){
+                    
+                    return resultado;
+                    
+                } 
+                
+                //Verifica se a matriz tem apenas um elemnento
+                else if(matrix[0].length == 1){
+                    resultado[0] = matrix[0][0];
+                    
+                    return resultado;
+                } 
+
+                //Verifica se a matriz tem mais do que um elemnento
+                else if( matrix[0].length > 1){
+
+                    int n = matrix.length;
+                    int limiteSuperior = 0;
+                    int limiteInferior = n - 1;
+                    int limiteEsquerdo = 0;
+                    int limiteDireito = n - 1;
+
+                    while (limiteSuperior <= limiteInferior && limiteEsquerdo <= limiteDireito) {
+
+                        // Percorrendo o limite superior da matriz
+                        for (int i = limiteEsquerdo; i <= limiteDireito; i++) {
+                            resultado[count] = matrix[limiteSuperior][i];
+                            count++;
+                        }
+                        limiteSuperior++;
+
+                        // Percorrendo o limite direito da matriz
+                        for (int i = limiteSuperior; i <= limiteInferior; i++) {
+                            resultado[count] = matrix[i][limiteDireito];
+                            count++;
+                            
+                        }
+                        limiteDireito--;
+
+                        // Percorrendo o limite inferior da matriz (em sentido reverso)
+                        if (limiteSuperior <= limiteInferior) {
+                            for (int i = limiteDireito; i >= limiteEsquerdo; i--) {
+                                resultado[count] = matrix[limiteInferior][i];
+                                count++;
+                                
+                            }
+                            limiteInferior--;
+                        }
+
+                        // Percorrendo o limite esquerdo da matriz (em sentido reverso)
+                        if (limiteEsquerdo <= limiteDireito) {
+                            for (int i = limiteInferior; i >= limiteSuperior; i--) {
+                                resultado[count] = matrix[i][limiteEsquerdo];
+                                count++;
+                                
+                            }
+                            limiteEsquerdo++;
+                        }
+                        }
+                       
+                };
+                
+                return resultado;
+              
+        };
+        };
+
+        Future<int[]> resultado = executor.submit(task);
+        executor.shutdown();
+        return resultado;
   };
 }
