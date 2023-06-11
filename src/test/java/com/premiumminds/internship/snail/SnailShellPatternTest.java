d052f38547469250e34f517dfae2ffa21d0b937b
Heeie <114531189+Heeie@users.noreply.github.com>
2023-06-11 18:58:06 +0100

    Update SnailShellPatternTest.java

--- src/test/java/com/premiumminds/internship/snail/SnailShellPatternTest.java
+++ src/test/java/com/premiumminds/internship/snail/SnailShellPatternTest.java
@@ -27,105 +27,68 @@ public class SnailShellPatternTest extends SnailShellPattern{
    *
    */
   public SnailShellPatternTest() {
-      int[][] teste0 = new int[][] {{}};
-        
-      int[][] teste1 = new int[][] { { 1 } };
-
-      int[][] teste2 = new int[][] {{ 1, 2, 3 },
-                                    { 4, 5, 6 },
-                                    { 7, 8, 9 } };
-
-      int[][] teste3 = new int[][] {{ 1, 2, 3 , 4},
-                                    { 5, 6, 7, 8 },
-                                    {9, 10, 11, 12 },
-                                    { 13, 14, 15, 16}};
-
-      int[][] teste4 = new int[][] { { 1, 2},
-                                     { 3 ,4}};
-
-      SnailShellPattern tester = new SnailShellPattern();
-
-      System.out.println(tester.getSnailShell(teste0));
-      System.out.println(tester.getSnailShell(teste1));
-      System.out.println(tester.getSnailShell(teste2));
-      System.out.println(tester.getSnailShell(teste3));
-      System.out.println(tester.getSnailShell(teste4));
   };
 
   @Test
   public void ScreenLockinPatternTestFirst3Length2Test()
       throws InterruptedException, ExecutionException, TimeoutException {
-    int[][] matrix = { { 1, 2, 3 }, { 8, 9, 4 }, { 7, 6, 5 } };
+    
+    int[][] matrix = new int[][] {{ 1, 2, 3 },
+                                      { 4, 5, 6 },
+                                      { 7, 8, 9 } };
     Future<int[]> count = new SnailShellPattern().getSnailShell(matrix);
     int[] result = count.get(10, TimeUnit.SECONDS);
-    int[] expected = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    int[] expected = { 1, 2, 3, 6, 9, 8, 7, 4, 5 };
     assertArrayEquals(result, expected);
   }
+
+  @Test
+  public void ScreenLockinPatternTest1Length2Test()
+      throws InterruptedException, ExecutionException, TimeoutException {
+    int[][] matrix = { {} };
+    Future<int[]> count = new SnailShellPattern().getSnailShell(matrix);
+    int[] result = count.get(10, TimeUnit.SECONDS);
+    int[] expected = {};
+    assertArrayEquals(result, expected);
+  }
+
+  @Test
+  public void ScreenLockinPatternTest2Length2Test()
+      throws InterruptedException, ExecutionException, TimeoutException {
+    int[][] matrix = { { 1, 2 }, { 3, 4 } };
+    Future<int[]> count = new SnailShellPattern().getSnailShell(matrix);
+    int[] result = count.get(10, TimeUnit.SECONDS);
+    int[] expected = { 1, 2, 4, 3 };
+    assertArrayEquals(result, expected);
+  }
+
+  @Test
+  public void ScreenLockinPatternTest4Length2Test()
+      throws InterruptedException, ExecutionException, TimeoutException {
+    int[][] matrix = new int[][] {{ 1, 2, 3 , 4},
+                                  { 5, 6, 7, 8 },
+                                  {9, 10, 11, 12 },
+                                  { 13, 14, 15, 16}};
+    Future<int[]> count = new SnailShellPattern().getSnailShell(matrix);
+    int[] result = count.get(10, TimeUnit.SECONDS);
+    int[] expected = { 1, 2, 3, 4, 8, 12, 16, 15, 14, 13, 9, 5, 6, 7, 11, 10};
+    assertArrayEquals(result, expected);
+  }
+
+  @Test
+  public void ScreenLockinPatternTest7Length2Test()
+      throws InterruptedException, ExecutionException, TimeoutException {
+    int[][] matrix = { { 1 , 2 , 3 , 4 , 5 , 6 , 7 }, 
+                       { 24, 25, 26, 27, 28, 29, 8 }, 
+                       { 23, 40, 41, 42, 43, 30, 9 }, 
+                       { 22, 39, 48, 49, 44, 31, 10 }, 
+                       { 21, 38, 47, 46, 45, 32, 11 }, 
+                       { 20, 37, 36, 35, 34, 33, 12 }, 
+                       { 19, 18, 17, 16, 15, 14, 13 } };
+    Future<int[]> count = new SnailShellPattern().getSnailShell(matrix);
+    int[] result = count.get(10, TimeUnit.SECONDS);
+    int[] expected = new int[49];
+    for (int i = 0; i<49; i++) {expected[i]=i+1;}
+    assertArrayEquals(result, expected);
 }
-
-class SnailShellPattern{
-    /*
-     * Recebe uma matriz NxN e ordena os seus 
-     * elementos organizados, dos mais exteriores 
-     * para os interiores, percorrendo a matriz no sentido horário. 
-     * 
-     * @param matrix  matriz de números inteiros
-     * @return lista com os elementos da matrix ordenados
-     * 
-     */
-    public List<Integer> getSnailShell(int[][] matrix) {
-        
-        int[][] vazio = new int[][] {{}};
-        List<Integer> resultado = new ArrayList<>();
-    
-        //Verifica se a matriz é vazia
-        if(vazio == matrix){
-            return resultado;
-        } 
-        
-        //Verifica se a matriz tem apenas um elemnento
-        else if(matrix[0].length == 1){
-            resultado.add( matrix[0][0]);
-            return resultado;
-        } 
-
-        //Verifica se a matriz tem mais do que um elemnento
-        else if( matrix[0].length > 1){
-
-            int n = matrix.length;
-            int limiteSuperior = 0;
-            int limiteInferior = n - 1;
-            int limiteEsquerdo = 0;
-            int limiteDireito = n - 1;
-
-            while (limiteSuperior <= limiteInferior && limiteEsquerdo <= limiteDireito) {
-
-                for (int i = limiteEsquerdo; i <= limiteDireito; i++) {
-                    resultado.add(matrix[limiteSuperior][i]);
-                }
-                limiteSuperior++;
-
-                for (int i = limiteSuperior; i <= limiteInferior; i++) {
-                    resultado.add(matrix[i][limiteDireito]);
-                }
-                limiteDireito--;
-
-                
-                if (limiteSuperior <= limiteInferior) {
-                    for (int i = limiteDireito; i >= limiteEsquerdo; i--) {
-                        resultado.add( matrix[limiteInferior][i]);
-                    }
-                    limiteInferior--;
-                }
-
-                if (limiteEsquerdo <= limiteDireito) {
-                    for (int i = limiteInferior; i >= limiteSuperior; i--) {
-                        resultado.add(matrix[i][limiteEsquerdo]);
-                    }
-                    limiteEsquerdo++;
-                }
-                }
-            }
-            return resultado;
-        }
 }
